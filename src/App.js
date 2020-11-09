1f1fb3d8e11f47f975835a940b41019cb613fa78
ojedamartisof <ojedamartinasofia@gmail.com>
2020-11-09 14:28:13 -0300

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styleTop = {
+  color: ' color: #e6ff99;}', //h1 style
+  fontSize: "100px",
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+  render() {
+    return (
+
+      <div className="container justify-content-center">
+        <div className="row justify-content-center">
+          <h1 style={styleTop}><i>Todo List</i></h1>
+        </div>
+        <TodoList />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+const form = {
+    width: "70%",
+}
+const parag = {
+    color: "grey",
+    fontSize: "14px",
+    marginBottom: "0",
+    marginLeft: "10px"
+}
+class TodoList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            tasks: []
+        }
+    }
+    addTask(e) {
+        if (e.keyCode == 13 && e.target.value !== '') {
+            this.setState({
+                tasks: this.state.tasks.concat(e.target.value)
+            })
+            e.target.value = '';
+        }
+    }
+    delTask(i) {
+        const tasks = this.state.tasks;
+        tasks.splice(i, 1);
+        this.setState({
+            tasks: tasks
+        })
+    }
+
+    delButton(i) {
+        const tasks = this.state.tasks;
+        tasks.splice(i);
+        this.setState({
+            tasks: tasks
+        })
+
+    }
+    render() {
+        return (
+            <div className="card m-auto paper" style={form}>
+                <div class="input-group">
+                    <input id="addToDo" type="text" className="form-control" placeholder="What needs to be done?"
+                        onKeyDown={(e) => this.addTask(e)} />
+                    <div className="input-group-append" id="button-addon4">
+                        <button className="btn btn-outline-secondary" onClick={(i) => this.delButton(i)} type="button">Delete</button>
+                    </div>
+                </div>
+                {
+                    this.state.tasks.length > 0 ? (
+                        <ul id="list">
+                            {
+                                this.state.tasks.map((task, i) => {
+                                    return (
+                                        <li>
+                                            {task}
+                                            <span><i className="fa fa-times float-right" onClick={() => this.delTask(i)}></i></span>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    ) : (
+                            <p style={parag}>Add a task</p>
+                        )
+                }
+                <p id="parag" style={parag}></p>
+            </div>
+        )
+    }
+}
+
+
+export default TodoList;
